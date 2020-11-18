4c6a07306d01eff62938a8d874088b4f96204bc9
denitze <denise.brachwitz@gmail.com>
2020-11-18 19:20:11 +0100

    new API Denise

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,41 +12,40 @@ let ingredient = document.getElementById("ingredient")
 
     function findRecipe() {
         ingredient = ingredient.value;
-        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=dc19a07e2b204a24857d8eff71c69d5b&number=3&ingredients=${ingredient}`)
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=df611f75b97141be917063ad735d8e66&number=6&ingredients=${ingredient}`)
         .then(response => response.json())
         .then(json => {
-        if(json.length < 1) {
+        if(ingredient = json.length < 1) {
             console.log("ERROR");
+            foodResult.innerHTML +="Hierzu gibt es kein Rezept!"
         } else {
 
         for (let i=0; i<json.length; i++) {
             console.log(json)
             let recipeId = json[i].id;
 
-            fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=dc19a07e2b204a24857d8eff71c69d5b`)
+            fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=df611f75b97141be917063ad735d8e66`)
             .then(response => response.json())
             .then(json => { 
                 foodResult.innerHTML +=
                 `<div class="recipe">
             <img src="${json.image}" alt="">
             <h2>${json.title}</h2>
+            <img src="assets/img/cooking-time.png" alt="" class="icon">
             <p> Ready in ${json.readyInMinutes} Minutes</p>
             <button><a href="${json.sourceUrl}">See Recipe</a></button>
         </div>`
-                console.log(json);
-                console.log(json.title)
-                console.log(json.image)
-                console.log(json.readyInMinutes);
-                console.log(json.summary);
-                console.log(json.sourceUrl);
-                console.log("------");
             })
            
         }   
-        ingredient = " "
         }
+        
     })
+    ingredient = " "
+
     }
+
+
 // Finns JS
 const newsGrid = document.getElementById("newsGrid")
 
@@ -70,7 +69,6 @@ fetch('http://newsapi.org/v2/top-headlines?country=de&category=business&apiKey=0
                 <img src=${element.urlToImage} alt="">
                 <figcaption>${element.title}</figcaption>
                 <p class="description">${element.description} <a href=${element.url}>Weiter lesen</a></p>
-                
             </figure>
             `
         }
